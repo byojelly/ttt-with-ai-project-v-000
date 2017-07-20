5a220868a3597d131af6bbf6677678be3682a621
Seb <sebgertz@gmail.com>
2017-07-20 20:44:26 +0100

    Update CLI.rb

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,26 +1,36 @@
 class CLI
 
   def call
-      puts "Would you like to play with 0, 1, or 2 players?"
-        number_of_players = gets.strip.to_i
-      if number_of_players == 1
-        puts "Should player 1 be a computer or a human?"
-        answer = gets.strip.downcase
-      end
-
-      if number_of_players == 0
-        game = Game.new(player_1 = Players::Computer.new("X"), player_2 = Players::Computer.new("O"), Board.new)
-      elsif number_of_players == 1
-        if answer == "computer"
-          game = Game.new(player_1 = Players::Computer.new("X"), player_2 = Players::Human.new("O"), Board.new)
-        elsif answer == "human"
-          game = Game.new(player_1 = Players::Human.new("X"), player_2 = Players::Computer.new("O"), Board.new)
-        end
-      else
-        game = Game.new(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), Board.new)
-      end
-
-      game.play
+    #number of players
+    puts "Would you like to play with 0, 1, or 2 players?"
+    number_of_players = gets.strip.to_i
+    #find out which token starts
+    puts "Who should go first, 'X' or 'O'?"
+    starting_token = gets.strip
+      
+    if starting_token == 'O'
+      opposing_token = 'X'
+    else
+      opposing_token = 'O'
     end
+
+    #set up game
+    if number_of_players == 0
+        game = Game.new(Players::Computer.new(starting_token), Players::Computer.new(opposing_token), Board.new)
+    elsif number_of_players == 1
+      puts "Should player 1 be a computer or a human?"
+      answer = gets.strip.downcase
+      
+      if answer == "computer"
+          game = Game.new(Players::Computer.new(starting_token), Players::Human.new(opposing_token), Board.new)
+      elsif answer == "human"
+          game = Game.new(Players::Human.new(starting_token), Players::Computer.new(opposing_token), Board.new)
+      end
+    else
+      game = Game.new(Players::Human.new(starting_token), Players::Human.new(opposing_token), Board.new)
+    end
+
+    game.play
+  end
 
 end
